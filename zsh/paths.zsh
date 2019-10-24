9e8c4ffae1a4be4c226dc2aeccd015547ef7eb00
IMAMASU Ryohei <imms.ryohei@gmail.com>
2019-10-25 01:15:14 +0900

    Add GOPATH and Fixes styles

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -4,13 +4,21 @@
 
 # Homebrew
 if [[ "${OSTYPE}" == "darwin"* ]]; then
-    export PATH="/usr/local/sbin:$PATH"
+    export PATH="/usr/local/sbin:${PATH}"
 fi
 
 # Python via pyenv-virtualenv
-export PYENV_ROOT=${HOME}/.pyenv
+export PYENV_ROOT="${HOME}/.pyenv"
 if [ -d "${PYENV_ROOT}" ]; then
-    export PATH=${PYENV_ROOT}/bin:$PATH
-        eval "$(pyenv init -)"
+    export PATH="${PYENV_ROOT}/bin:${PATH}"
+    eval "$(pyenv init -)"
     eval "$(pyenv virtualenv-init -)"
+fi
+
+# Go
+if [[ "${OSTYPE}" == "darwin"* ]]; then
+    if type go > /dev/null 2>&1; then
+        export GOPATH="${HOME}/.go"
+        export PATH="${GOPATH}/bin:${PATH}"
+    fi
 fi
