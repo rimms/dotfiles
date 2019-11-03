0ed585ad1cc798d14bf2cfcddb2013ba9f2bb5a8
IMAMASU Ryohei <imms.ryohei@gmail.com>
2019-11-03 12:51:29 +0900

    Add alias of selector

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -25,10 +25,30 @@ alias mkdir='mkdir -p'
 
 alias less='less -R'
 
+# Git repositories managed by ghq
 if type ghq > /dev/null 2>&1; then
     if type peco > /dev/null 2>&1; then
-        alias gcd='cd $(ghq list -p | peco --prompt "GIT REPOSITORY>")'
+        function ghq-repository-selector() {
+            ghq list -p | peco --prompt "GIT REPOSITORY>"
+        }
+        alias -g git_repos='$(ghq-repository-selector)'
     fi
+fi
+
+# Docker contaoners and images
+if type peco > /dev/null 2>&1; then
+    function docker-container-selector() {
+        docker ps --all --format "{{.ID}}\t{{.Names}}\t<{{.Status}}>" | \
+            peco --prompt "DOCKER CONTAINER>" | \
+            awk -F" " '{print $1}'
+    }
+    function docker-image-selector() {
+        docker images --format "{{.ID}}\t{{.Repository}}:{{.Tag}}\t<{{.CreatedAt}}>" | \
+            peco --prompt "DOCKER IMAGE>" | \
+            awk -F" " '{print $1}'
+    }
+    alias -g docker_containers='$(docker-container-selector)'
+    alias -g docker_images='$(docker-image-selector)'
 fi
 
 function history-all {
